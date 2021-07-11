d40a80e2c3d44ef7050885a75f8e55229c9453c8
VladGoogle <71459589+VladGoogle@users.noreply.github.com>
2021-07-11 17:39:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,6 @@ function writeToFile() {
 
     process.argv;
         fs.appendFileSync(process.argv[2], process.argv.slice(3).join(' '));
-        /*fs.appendFile("hello.txt", val, function (error) {
-            if (error) throw error; // если возникла ошибка*/
-            /*console.log("Запись файла завершена. Содержимое файла:");
-            let data = fs.readFileSync(process.argv[2], "utf8");
-            console.log(data);*/
 }
 
 writeToFile();
